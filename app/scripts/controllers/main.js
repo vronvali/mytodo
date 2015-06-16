ab8d01dd96b034327aa34b50ed6d24a21f2f914a
vronvali <vronvali@cesine.ca>
2015-06-16 11:35:40 -0400

    Adding Local Storage
    Here we added local storage space and defined it

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the mytodoApp
  */
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+  	var todosInStore =localStorageService.get('todos');
+  	$scope.todos =todosInStore || [];
+
+  	$scope.$watch('todos',function(){
+  		localStorageService.set('todos', $scope.todos);
+  	}, true);
+    $scope.addTodo = function(){
+    	$scope.todos.push($scope.todo);
+    	$scope.todo =' ';
+    };
+    $scope.removeTodo = function(index) {
+    	$scope.todos.splice(index, 1);
+    };
   });
